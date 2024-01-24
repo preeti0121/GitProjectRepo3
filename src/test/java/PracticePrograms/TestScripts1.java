d8518dd2816a98c2be65073b44eaa67df27eb585
Preeti Pawar <preepawar@deloitte.com>
2024-01-24 13:37:10 +0530

    TC_0001 merge to master branch

---
+++ src/test/java/PracticePrograms/TestScripts1.java
@@ -0,0 +1,14 @@
+package PracticePrograms;
+
+public class TestScripts1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test script1");
+		System.out.println("Create local branch and push changes to local branch");
+		System.out.println("Raise a PR");
+
+	}
+
+}
